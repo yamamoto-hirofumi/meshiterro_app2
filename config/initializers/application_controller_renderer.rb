8b350dc6efb0620f13e2a3b0789461a5fc21f860
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-07-22 06:37:35 +0000

    [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '694fcc208604a4fe06f7d3d4bd6fa2abb7e43fb9bb313107a1c4af0bb7ae2c4e6add42ee5230c49e921c611009c3cab4280a71b9d12224e693fa3d32b192e987'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
